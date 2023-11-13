8e00486064ffe8ca60d7eed4c0efd35a85acf29a
Ami Nice <amimakeup@gmail.com>
2023-11-13 16:23:53 +0200

    alert test

--- src/homepage.ts
+++ src/homepage.ts
@@ -27,3 +27,52 @@ ajax.onload = function(): void{
 };
 ajax.send();
 
+// const flashContainer = document.getElementById("flash-container")!;
+
+// interface Flash {
+//   timestamp: Date;
+//   message: string;
+// }
+
+// function displayFlash(flash: Flash): void {
+//   const currentTime = new Date();
+//   const flashTime = new Date(flash.timestamp);
+
+//   const flashText = `Flash at ${flashTime.toLocaleTimeString()} (${getTimeDifference(currentTime, flashTime)}) ago: ${flash.message}`;
+
+//   const flashElement = document.createElement("div");
+//   flashElement.textContent = flashText;
+
+//   flashContainer.appendChild(flashElement);
+// }
+
+// function getTimeDifference(currentTime: Date, flashTime: Date): string {
+//   const diff = currentTime.getTime() - flashTime.getTime();
+//   const secondsDiff = Math.floor(diff / 1000);
+
+//   if (secondsDiff < 60) {
+//     return `${secondsDiff} seconds`;
+//   } else if (secondsDiff < 3600) {
+//     return `${Math.floor(secondsDiff / 60)} minutes`;
+//   } else {
+//     return `${Math.floor(secondsDiff / 3600)} hours`;
+//   }
+// }
+
+// function startFlashes(): void {
+//   const flashes: Flash[] = [
+//     { timestamp: new Date("2023-11-13T14:00:00"), message: "Very Imported!!!" },
+//     { timestamp: new Date("2023-11-13T16:05:00"), message: "Now in the army!" },
+//     { timestamp: new Date("2023-11-13T16:10:00"), message: "Go to school!" },
+//   ];
+
+//   flashes.forEach((flash, index) => {
+//     setTimeout(() => {
+//       displayFlash(flash);
+//     }, index * 5000); // Adjust the interval between flashes (in milliseconds)
+//   });
+// }
+
+// startFlashes();
+
+
