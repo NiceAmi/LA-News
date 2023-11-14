52f917a02ff51adab9d50ecfaaff090f32d4a54b
Liel Buchnik <lielb45@gmail.com>
2023-11-14 13:07:40 +0200

    CSS update

--- build/scripts/fullArticle.js
+++ build/scripts/fullArticle.js
@@ -8,20 +8,21 @@ if (selectedPost) {
         console.log(response);
         let post = response.articles.find((article) => article.articleName === selectedPost);
         let news = `
-        <ul>
-        <li>${post.articleName}</li>
-        <li>${post.ShortArticle}</li>
-        <li><img src="${post.picture}" alt="Article Image"></li>
-        <li>${post.fullArticle}</li>
-        <li>${post.Reporter}</li>
-        </ul>`;
+        <div></div>
+        <div class="article-container">
+        <h1 class="post-article">${post.articleName}</h1>
+        <h4 class="under-title">${post.ShortArticle}</h4>
+        <p class=-"reporter-name">${post.Reporter}</p>
+        <p class="article-main-text">${post.fullArticle}</p>
+        </div>`;
         $("#fullPost").append(news);
     };
     ajax.send();
 }
 else {
-    window.location.href = "https://www.youtube.com/";
+    window.location.href = "./homepage.html";
 }
 $("#back").on("click", function () {
     window.location.href = "./homepage.html";
 });
+// <div class="post-main-image"><img src="${post.picture}" alt="Article Image"></div>
